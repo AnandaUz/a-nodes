09e2e7135c2b1200d2918f76398d78e6c53ec507
angel <angel.yarkiy@gmail.com>
2026-06-27 20:21:41 +0500

    +

--- client/src/pages/welcome/welcome.ts
+++ client/src/pages/welcome/welcome.ts
@@ -6,9 +6,24 @@ export function welcomePage() {
     html,
     title: "Добро пожаловать — Liner",
     init() {
-      document.querySelector(".google-btn")?.addEventListener("click", () => {
-        (window as any).google?.accounts?.id?.prompt();
-      });
+      // document.querySelector(".google-btn")?.addEventListener("click", () => {
+      //   (window as any).google?.accounts?.id?.prompt((notification: any) => {
+      //     console.log("isNotDisplayed:", notification.isNotDisplayed());
+      //     console.log(
+      //       "getNotDisplayedReason:",
+      //       notification.getNotDisplayedReason(),
+      //     );
+      //     console.log("isSkippedMoment:", notification.isSkippedMoment());
+      //     console.log("getSkippedReason:", notification.getSkippedReason());
+      //     console.log("isDismissedMoment:", notification.isDismissedMoment());
+      //     console.log("getDismissedReason:", notification.getDismissedReason());
+      //   });
+      // });
+
+      (window as any).google?.accounts?.id?.renderButton(
+        document.querySelector(".for-google-btn"),
+        { theme: "outline", size: "large" },
+      );
     },
   };
 }
